b63ee8c48572e8563f2b528c456bfffd01ea98d0
ALZOUBI <twisrmann20021@gmail.com>
2026-08-14 22:21:19 +0300

    fix: return JSON 401 on invalid JWT instead of HTML 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,28 +3,35 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (TokenExpiredException $e) {
+            return response()->json(['message' => 'Token has expired'], 401);
+        });
+
+        $this->renderable(function (TokenInvalidException $e) {
+            return response()->json(['message' => 'Token is invalid'], 401);
+        });
+
+        $this->renderable(function (JWTException $e) {
+            return response()->json(['message' => 'Token not provided'], 401);
         });
     }
 }
